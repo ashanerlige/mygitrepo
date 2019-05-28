b0f4180cc099a93410bf22d37d38f7b40c2e7eb3
ashanerlige <ashanh.ubdt@gmail.com>
2019-05-28 12:11:38 +0530

    my second commit for shob

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM openjdk:7
-MAINTAINER ashanh
-COPY MyFirstApp.java /usr/src/myapp/ 
-WORKDIR /usr/src/myapp/
-RUN javac MyFirstApp.java
-CMD ["java", "MyFirstApp"]
-
--- MyFirstApp.java
+++ MyFirstApp.java
@@ -2,6 +2,6 @@ class MyFirstApp
 { 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello >>>>>>>>>>, World"); 
+        System.out.println("Hello >>>>>>>>>>, World...............shobana"); 
     }  
 }
