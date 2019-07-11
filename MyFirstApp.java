f44d78ec4018eb84de64b2e652986709a85cf0f6
ashanerlige <50104944+ashanerlige@users.noreply.github.com>
2019-07-11 18:04:26 +0530

    Update MyFirstApp.java
    To check Jenkins integration

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -3,5 +3,6 @@ class MyFirstApp
     public static void main(String args[]) 
     { 
         System.out.println("Hello >>>>>>>>>>, World..........After adding Docker file....."); 
+        System.out.println("Hello >>>>>>>>>>, World..........After adding Docker file.....checking Jenkins integration");
     }  
 }
